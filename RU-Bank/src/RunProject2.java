e1a200c4afefa87fce5d12dc57096f82fa7c1507
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-10-15 21:29:55 -0400

    added comments

--- RU-Bank/src/RunProject2.java
+++ RU-Bank/src/RunProject2.java
@@ -1,6 +1,20 @@
 import java.io.FileNotFoundException;
-
+/**
+ * This is the entry point for the Project2 application.
+ * It creates an instance of the TransactionManager and invokes its run method to
+ * process transactions.
+ * 
+ * Arun Felix, Digvijay Singh
+ */
 public class RunProject2 {
+
+    /**
+     * The main method to start the application.
+     * 
+     * @param args command-line arguments (not used in this application).
+     * @throws FileNotFoundException if the input file for transactions cannot be found.
+     * @throws InterruptedException if the thread sleep in TransactionManager is interrupted.
+     */
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         new TransactionManager().run();
     }
